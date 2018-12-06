b015aae7e6f41a694f5530bf24fd861889819ba2
jacobcolegio-wmich <44441120+jacobcolegio-wmich@users.noreply.github.com>
2018-12-05 21:33:25 -0500

    Jacob - wrote constructor and readCourseFile()

--- CS1120.LA7/src/edu/wmich/cs1120/LA7/Controller.java
+++ CS1120.LA7/src/edu/wmich/cs1120/LA7/Controller.java
@@ -1,41 +1,66 @@
 package edu.wmich.cs1120.LA7;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Controller implements IController{
 	
-	BufferedReader FileIn;
-	BufferedReader FileIN1;
-	Scanner cou;
-	Scanner req;
+	LinkedList<Course> myCourses = new LinkedList<Course>();
+	PriorityQueue<Request> myRequests = new PriorityQueue<Request>();
+	BufferedReader courseFileBR;
+	BufferedReader requestFileBR;
+	Scanner courseScan;
+	Scanner requestScan;
 	
 	public Controller(PriorityQueue<Request> requestQueue, LinkedList<Course> courses,
 			BufferedReader fileIn, BufferedReader fileIn1) {
-		FileIn = fileIn;
-		FileIN1 = fileIn1;
 		
+		myCourses = courses;
+		myRequests = requestQueue;
+		courseFileBR = fileIn;
+		requestFileBR = fileIn1;
 	}
-	
 	
 	@Override
 	public void readCourseFile() {
-		// TODO Auto-generated method stub
-	 cou = new Scanner(FileIn);
-	 
+		 
+		//try/catch for buffered reader
+		try {
+			
+			//Strings used for reading input
+			String currentLine = "";
+			String[] lineArray; 
+			
+			//Read current line of course.txt
+			while((currentLine = courseFileBR.readLine()) != null) {
+				
+				//Read the comma-separated values from the current line and store in linked list
+				lineArray = currentLine.split(",");
+				myCourses.add(new Course(lineArray[0], lineArray[1], lineArray[2]));
+			}
+			courseFileBR.close();
+			
+		}catch(IOException e) {
+			
+			System.out.println("Error: Problem reading course file.");
+		}
 	}
 
 	@Override
 	public void readRequestFile() {
-		// TODO Auto-generated method stub
-		req = new Scanner(FileIN1);
+		
+		requestScan = new Scanner(requestFileBR);
 		int i = 1;
-		while (req.hasNextLine()) {
+		
+		while (requestScan.hasNextLine()) {
 			i++;
 		}
+		
 		String[] reqArr = new String[i];
+		
 		for (int j = 0; j < i; j++) {
-			reqArr[i] = req.nextLine();
+			reqArr[i] = requestScan.nextLine();
 		}
 	}
 
@@ -49,6 +74,7 @@ public class Controller implements IController{
 	public void processRequests() {
 		// TODO Auto-generated method stub
 		boolean processed = false;
+		
 		if (!processed) {
 			System.out.println();
 		}
@@ -65,5 +91,4 @@ public class Controller implements IController{
 		// TODO Auto-generated method stub
 		
 	}
-
 }
